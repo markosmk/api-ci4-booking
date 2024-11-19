af39885f9f90b883605e2ac233d0bb137a1d0952
Markos Mk <markosmk@me.com>
2024-11-19 15:00:16 -0300

    update: add base controller for rest api class

---
+++ app/Controllers/ResourceBaseController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\RESTful\ResourceController;
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Extends ResourceController to add common functionality to all RESTful controllers
+ */
+abstract class ResourceBaseController extends ResourceController
+{
+    protected $helpers = [];
+
+    public function initController(RequestInterface $request, ResponseInterface $response, LoggerInterface $logger)
+    {
+        // Call to ResourceController::initController()
+        parent::initController($request, $response, $logger);
+
+        // load helpers
+        helper($this->helpers);
+    }
+
+    /**
+     * Method to respond with a server error
+     */
+    protected function respondWithServerError($message = 'Ha ocurrido un error en el servidor', )
+    {
+        return $this->response->setStatusCode(500)->setJSON([
+            'error'   => true,
+            'message' => $message,
+        ]);
+    }
+
+    protected function respondWithError($message = 'Ha ocurrido un error', $code = 500)
+    {
+        return $this->response->setStatusCode($code)->setJSON([
+            'error'   => true,
+            'message' => $message,
+        ]);
+    }
+}
