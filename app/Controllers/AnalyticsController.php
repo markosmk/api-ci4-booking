73bb5bd42034dcffc9831fa2115090d3874871ba
Markos Mk <markosmk@me.com>
2024-11-25 08:40:41 -0300

    update: separe recent bookings and add cache

--- app/Controllers/AnalyticsController.php
+++ app/Controllers/AnalyticsController.php
@@ -8,6 +8,16 @@ class AnalyticsController extends ResourceBaseController
 {
     public function index()
     {
+        $cache = \Config\Services::cache();
+        $cacheKey = 'dashboard_stats';
+
+        // try to get data from cache
+        $cachedData = $cache->get($cacheKey);
+
+        if ($cachedData) {
+            return $this->response->setJSON($cachedData);
+        }
+
         $db = \Config\Database::connect();
 
         $totalIncome = $db->table('bookings')
@@ -38,22 +48,22 @@ class AnalyticsController extends ResourceBaseController
             ->countAllResults();
 
         // last `10` pending bookings
-        $recentPendingBookings = $db->table('bookings')
-            ->select('
-                bookings.id AS booking_id,
-                bookings.quantity,
-                bookings.status,
-                bookings.created_at AS booking_created_at,
-                customers.name AS customer_name,
-                tours.name AS tour_name
-                ')
-            ->join('customers', 'customers.id = bookings.customerId')
-            ->join('tours', 'tours.id = bookings.tourId')
-            ->where('bookings.status', 'PENDING')
-            ->orderBy('bookings.created_at', 'DESC')
-            ->limit(10)
-            ->get()
-            ->getResultArray();
+        // $recentPendingBookings = $db->table('bookings')
+        //     ->select('
+        //         bookings.id AS booking_id,
+        //         bookings.quantity,
+        //         bookings.status,
+        //         bookings.created_at AS booking_created_at,
+        //         customers.name AS customer_name,
+        //         tours.name AS tour_name
+        //         ')
+        //     ->join('customers', 'customers.id = bookings.customerId')
+        //     ->join('tours', 'tours.id = bookings.tourId')
+        //     ->where('bookings.status', 'PENDING')
+        //     ->orderBy('bookings.created_at', 'DESC')
+        //     ->limit(10)
+        //     ->get()
+        //     ->getResultArray();
 
         // monthly income by months
         $monthlyIncome = $db->table('bookings')
@@ -68,15 +78,50 @@ class AnalyticsController extends ResourceBaseController
             ->get()
             ->getResultArray();
 
-        return $this->response->setJSON([
+        $data = [
             'total_income' => $totalIncome ?? 0,
             'total_visitors' => $totalVisitors ?? 0,
             'total_bookings_confirmed' => $totalBookingsConfirmed ?? 0,
             'total_bookings_not_pending' => $totalBookingsNotPending ?? 0,
             'total_active_tours' => $totalActiveTours ?? 0,
-            'recent_pending_bookings' => $recentPendingBookings,
+            // 'recent_pending_bookings' => $recentPendingBookings,
             'monthly_income' => $this->formatMonthlyIncome($monthlyIncome),
-        ]);
+        ];
+
+        // save in cache by 10 minutes (600 seconds)
+        $cache->save($cacheKey, $data, 600);
+
+        return $this->response->setJSON($data);
+    }
+
+    public function recentsBookings()
+    {
+        $db = \Config\Database::connect();
+        $bookings = $db->table('bookings')
+            ->select('
+                bookings.id AS booking_id,
+                bookings.quantity,
+                bookings.status,
+                bookings.created_at AS booking_created_at,
+                customers.name AS customer_name,
+                tours.name AS tour_name
+                ')
+            ->join('customers', 'customers.id = bookings.customerId')
+            ->join('tours', 'tours.id = bookings.tourId')
+            // ->where('bookings.status', 'PENDING')
+            ->orderBy('bookings.created_at', 'DESC')
+            ->limit(10)
+            ->get()
+            ->getResultArray();
+
+        return $this->response->setJSON($bookings);
+    }
+
+    public function clearCache()
+    {
+        $cache = \Config\Services::cache();
+        $cache->clean();
+        return $this->response->setJSON(['message' => 'Cache cleared']);
     }
 
     private function formatMonthlyIncome(array $data)
