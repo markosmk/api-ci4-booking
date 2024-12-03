0853ae7897465995299379620b1b88c49ae9e7ef
Markos Mk <markosmk@me.com>
2024-12-03 06:50:31 -0300

    update: add some new properties to tour

--- app/Models/TourModel.php
+++ app/Models/TourModel.php
@@ -9,7 +9,7 @@ class TourModel extends Model
     protected $table            = 'tours';
     protected $primaryKey       = 'id';
     protected $returnType       = 'array';
-    protected $allowedFields    = ['name', 'description', 'duration', 'capacity', 'active'];
+    protected $allowedFields    = ['name', 'description', 'content', 'media', 'duration', 'capacity', 'price', 'active'];
 
     // Dates
     protected $useTimestamps = true;
@@ -18,8 +18,12 @@ class TourModel extends Model
 
     protected $validationRules = [
         'name'        => 'required|min_length[3]',
-        'description' => 'required',
+        'description' => 'required|min_length[10]|max_length[200]',
+        'content'     => 'required|min_length[10]|max_length[1000]',
         'duration'    => 'required|integer',
         'capacity'    => 'required|integer',
+        'price'       => 'required|decimal',
+        'media'       => 'permit_empty|valid_url',
+        'active'      => 'permit_empty|in_list[0,1]',
     ];
 }
