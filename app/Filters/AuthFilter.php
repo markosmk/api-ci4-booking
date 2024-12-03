7f56143df0d52869d9597ff9dc4e758b124e2356
Markos Mk <markosmk@me.com>
2024-12-03 06:49:21 -0300

    update: adjust cors with a method unify in filters, why? because in filters cors is not added automatically

--- app/Filters/AuthFilter.php
+++ app/Filters/AuthFilter.php
@@ -3,6 +3,8 @@
 namespace App\Filters;
 
 // use App\Libraries\CustomRequest;
+
+use App\Libraries\CustomRequest;
 use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
@@ -12,19 +14,28 @@ use Firebase\JWT\Key;
 
 class AuthFilter implements FilterInterface
 {
+    /**
+     * Does the actual filtering
+     *
+     * @param CustomRequest $request
+     * @param array|null $arguments
+     */
     public function before(RequestInterface | IncomingRequest $request, $arguments = null)
     {
         $token = $request->getCookie('app_token');
 
         // if token not found in cookie then check header
         if (!$token) {
+
             $authHeader = $request->getHeaderLine('Authorization');
             if (!$authHeader) {
-                return service('response')->setJSON([
-                    'error'   => true,
-                    'message' => 'No tienes autorización para acceder a este recurso'
-                ])->setStatusCode(401);
+                return $this->unauthorizedResponse('No tienes autorización para acceder a este recurso');
             }
+            //     return service('response')->setJSON([
+            //         'error'   => true,
+            //         'message' => 'No tienes autorización para acceder a este recurso'
+            //     ])->setStatusCode(401);
+            // }
 
             // take the token
             $token = str_replace('Bearer ', '', $authHeader);
@@ -35,18 +46,36 @@ class AuthFilter implements FilterInterface
             $decoded = JWT::decode($token, new Key($key, 'HS256'));
 
             // save data user in request for future use
-            // TODO: adjust user in IncomingRequest
             $request->user = (array) $decoded;
         } catch (\Exception $e) {
-            return service('response')->setJSON([
-                'error' => true,
-                'message' => $e->getMessage()
-            ])->setStatusCode(401);
+
+            return $this->unauthorizedResponse($e->getMessage());
+            // return service('response')->setJSON([
+            //     'error' => true,
+            //     'message' => $e->getMessage()
+            // ])->setStatusCode(401);
         }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
         // not action required
+    }
+
+    private function unauthorizedResponse(string $message)
+    {
+        $response = service('response');
+
+        // Agrega los encabezados CORS a la respuesta
+        $response->setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+        $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->setHeader('Access-Control-Allow-Credentials', 'true');
+        $response->setStatusCode(401);
+
+        return $response->setJSON([
+            'error' => true,
+            'message' => $message
+        ]);
     }
 }
--- app/Filters/SuperAdminFilter.php
+++ app/Filters/SuperAdminFilter.php
@@ -15,27 +15,47 @@ class SuperAdminFilter implements FilterInterface
 
         // verify user is authenticated
         if (!$user) {
-            return service('response')
-                ->setJSON([
-                    'error'   => true,
-                    'message' => 'Acceso denegado. No tienes autorización para realizar estaacción.',
-                ])
-                ->setStatusCode(401);
+            return $this->unauthorizedResponse('Acceso denegado. No tienes autorización para acceder a este recurso.', 401);
+            // return service('response')
+            //     ->setJSON([
+            //         'error'   => true,
+            //         'message' => 'Acceso denegado. No tienes autorización para realizar estaacción.',
+            //     ])
+            //     ->setStatusCode(401);
         }
 
         // verify user is authenticated, and is superadmin
         if ($user['role'] !== 'superadmin') {
-            return service('response')
-                ->setJSON([
-                    'error'   => true,
-                    'message' => 'Acceso denegado. No tienes permiso para realizar esta acción.',
-                ])
-                ->setStatusCode(403);
+
+            return $this->unauthorizedResponse('Acceso denegado. No tienes autorización para realizar esta acción.', 403);
+            // return service('response')
+            //     ->setJSON([
+            //         'error'   => true,
+            //         'message' => 'Acceso denegado. No tienes permiso para realizar esta acción.',
+            //     ])
+            //     ->setStatusCode(403);
         }
     }
 
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
     {
 
+    }
+
+    private function unauthorizedResponse(string $message, int $statusCode = 401)
+    {
+        $response = service('response');
+
+        // Agrega los encabezados CORS a la respuesta
+        $response->setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+        $response->setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+        $response->setHeader('Access-Control-Allow-Credentials', 'true');
+        $response->setStatusCode($statusCode);
+
+        return $response->setJSON([
+            'error' => true,
+            'message' => $message
+        ]);
     }
 }
